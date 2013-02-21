c3c57aba2a724a20f24522770b2f2e3e4040ece1
buckmarble <forrestmarble@gmail.com>
2013-02-21 17:22:15 -0500

    Revert "Added kernel config option 'OMAP_SCREENOFF_MAXFREQ'."
    This reverts commit ccbd49d595652dae2cc5fc146cf59bed5d69498b.
    
    Conflicts:
    
    	arch/arm/mach-omap2/Kconfig
    	arch/arm/mach-omap2/omap2plus-cpufreq.c

--- BBK.sh
+++ BBK.sh
@@ -4,7 +4,7 @@
 # Path to toolchain
   cco=~/kernel/android-toolchain-eabi/bin/arm-eabi-
 # Path to build your kernel
-  k=~/kernel/elite_kernelAOSP
+  k=~/kernel/elite_kernelAOSP30x
 # Directory for the any kernel updater
   t=$k/tools/bbk
 # Export betas to your dropbox
